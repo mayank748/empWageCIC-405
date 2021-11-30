ffd8ba8ee702bd2156997a44d78a536954b210c1
Mayank Singh <smayank748@gmail.com>
2021-11-30 08:44:25 +0530

    [Mayank] calculated the emp wage for one day

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,9 +4,19 @@ echo "Welcome"
 
 isPresent=$((RANDOM%2))
 valueForPresent=1
+wagePerHour=20
+FullDayWorkingHour=8
+empWorkedHour=0
+totalSalary=0
+
 if [ $isPresent -eq $valueForPresent ]
 then
 	echo "Emp is present."
+	empWorkedHour=$FullDayWorkingHour
 else
 	echo "Emp is absent."
 fi
+
+totalSalary=$(($empWorkedHour*wagePerHour))
+echo "Total Salary for one day "$totalSalary
+
