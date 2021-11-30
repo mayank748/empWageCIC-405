7a4f0c481ae98ecce698349dd584a07b3fefe734
Mayank Singh <smayank748@gmail.com>
2021-11-30 09:27:15 +0530

    [Mayank] calculating the emp wage for part time

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,21 +2,43 @@
 
 echo "Welcome"
 
-isPresent=$((RANDOM%2))
+isPresent=$((RANDOM%3))
 valueForPresent=1
+valueForPartTime=2
 wagePerHour=20
+wagePerForPatTime=20
+FullDayPartTimeWorkingHour=8
 FullDayWorkingHour=8
 empWorkedHour=0
 totalSalary=0
+empWage=0
 
 if [ $isPresent -eq $valueForPresent ]
 then
 	echo "Emp is present."
 	empWorkedHour=$FullDayWorkingHour
+	empWage=$wagePerHour
 else
-	echo "Emp is absent."
+	if [ $isPresent -eq $valueForPartTime ]
+	then
+		echo "Emp is doing part time"
+		empWorkedHour=$FullDayPartTimeWorkingHour
+		empWage=$wagePerForPatTime
+	else
+		echo "Emp is absent."
+	fi
 fi
 
-totalSalary=$(($empWorkedHour*wagePerHour))
+function CalculatEmpWage(){
+	empWorked=$1
+	wagePerHour=$2
+	echo $(($empWorked*$wagePerHour))
+}
+
+if [ $isPresent -eq $valueForPresent ] || [ $isPresent -eq $valueForPartTime ]
+then
+	totalSalary=`CalculatEmpWage $empWorkedHour $empWage`	
+fi
+
 echo "Total Salary for one day "$totalSalary
 
