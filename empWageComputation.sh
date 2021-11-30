40e14c32c66339955fbd69508aae611f8f73f270
Mayank Singh <smayank748@gmail.com>
2021-11-30 08:34:45 +0530

    [Mayank] implementation of attendance for emp

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,12 @@
+#! /bin/bash/ -x
+
 echo "Welcome"
+
+isPresent=$((RANDOM%2))
+valueForPresent=1
+if [ $isPresent -eq $valueForPresent ]
+then
+	echo "Emp is present."
+else
+	echo "Emp is absent."
+fi
